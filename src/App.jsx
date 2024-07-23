6fa66964a43ad2399bc9848817cac54e8a556940
Walaa soudy <11410120201218@stud.cu.edu.eg>
2024-07-23 18:32:35 +0300

    upload

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css';
+import Register from './components/Auth/Register';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import { Toaster } from 'react-hot-toast';
+
+
+const router = createHashRouter(
+  [
+    { path: '/login', element: <Login />, },
+    { path: '/register', element: <Register />, }
+
+  ])
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster
+        position='bottom-right'
+        toastOptions={{
+          success: {
+            style: {
+              background: '#28a745',
+              color: '#fff',
+            },
+          },
+          error: {
+            style: {
+              background: '#dc3545',
+              color: '#fff',
+            },
+          },
+        }}
+      />
+    </>
+  );
+}
+
+export default App;
